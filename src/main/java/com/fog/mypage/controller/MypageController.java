a27fb8c2825e7f26a8453a1f7587240d62d4121a
minsol <minsol@DESKTOP-UJM3099>
2022-12-08 21:24:39 +0900

    민솔 URL 공유

--- src/main/java/com/fog/mypage/controller/MypageController.java
+++ src/main/java/com/fog/mypage/controller/MypageController.java
@@ -1,9 +1,12 @@
 package com.fog.mypage.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fog.config.auth.PrincipalDetails;
 
 import lombok.RequiredArgsConstructor;
 

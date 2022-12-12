d92ea4ce7d6d529357ee51f32fc287bf3d201295
김종하 <81893082+gbr7780@users.noreply.github.com>
2022-12-13 00:35:01 +0900

    종하 최초 회원가입시 카테고리 생성 완료

--- src/main/java/com/fog/mypage/entity/Category.java
+++ src/main/java/com/fog/mypage/entity/Category.java
@@ -1,12 +1,18 @@
 package com.fog.mypage.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fog.member.entity.Member;
 import com.fog.mypage.dto.CategoryUpdateDto;
 
 import lombok.AllArgsConstructor;
@@ -30,6 +36,26 @@ public class Category {
     // 카테고리 종류
     @Column(unique = true,name="category_type")
     private String type;
+    
+    // 다대일 관계 - 외래키(사용자)
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+    
+    
+    // 카테고리 생성 메소드
+    public static List<Category> createCategory() {
+    	String [] deType = {"프로필","프로젝트","대외활동","수상자격 / 자격증", "보유기술"};
+    	List<Category> categorys = new ArrayList<Category>();
+    	
+    	for (int i = 0; i < deType.length; i++) {
+    		Category category = new Category();
+        	category.setType(deType[i]);
+        	categorys.add(category);
+		}
+    	
+    	return categorys;
+    }
     
 
 }
